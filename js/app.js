d6e2f296cb17ec377ad268f4221a1ee83aa16d9c
Joe Hinkley <joehhinkley@gmail.com>
2019-01-08 11:19:08 -0800

    search bar accepts user input

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log($);
+const data = {
+  fetchVideos: (searchTerm) => {
+    console.log(searchTerm);
+  }
+}
+
+const video = {
+  search: (event) => {
+    event.preventDefault();
+    const $searchBar = $('#search-bar');
+    const searchTerm = $searchBar.val()
+
+    data.fetchVideos(searchTerm)
+  }
+};
+
+$(() => {
+  $('form.search').on('submit', video.search);
+})
